21213f04adff6da9d1ce81d6b9c2d4e8b74ba294
Sai Rohith Maddla <sai.rohith@innovatechs.com>
2026-03-23 23:25:46 +0530

    first commit

---
+++ Slot-machine/Slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_machineApp.swift
+//  Slot-machine
+//
+//  Created by Sai Rohith on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
